cb5cf5e0e632c7d0ea417129218ac464a59cc8d2
akshaysinghofficial007-ux <akshaysinghofficial007@gmail.com>
2026-09-05 11:39:09 +0530

    week 3 commited

---
+++ week 3/IdCard.java
@@ -0,0 +1,13 @@
+class IdCard {
+    String name; int booksIssued;
+    IdCard(String n,int b){name=n;booksIssued=b;}
+    public static void main(String[] args){
+        IdCard ravi=new IdCard("Ravi",0);
+        IdCard duplicate=ravi;
+        duplicate.booksIssued=3;
+        IdCard separate=new IdCard("Ravi",3);
+        System.out.println("Ravi booksIssued:"+ravi.booksIssued);
+        System.out.println("duplicate==ravi:"+(duplicate==ravi));
+        System.out.println("separate==ravi:"+(separate==ravi));
+    }
+}
