bc970bc7e572714d24011ee6b602d7034bc2d246
akshaysinghofficial007-ux <akshaysinghofficial007@gmail.com>
2026-09-05 11:42:03 +0530

    Delete week 1 directory

--- week 1/ReverseName.java
+++
@@ -1,7 +0,0 @@
-class ReverseName {
-    static String reverse(String s){return new StringBuilder(s).reverse().toString();}
-    public static void main(String[] args){
-        String name="Sunil";
-        System.out.println("Original="+name+" Reversed="+reverse(name));
-    }
-}
