bb4516f6e26254d140486bc57f6a65feb3887869
akshaysinghofficial007-ux <akshaysinghofficial007@gmail.com>
2026-09-12 00:11:35 +0530

    Delete week 2/session2 directory

--- week 2/session2/classproblem/CSVParser.java
+++
@@ -1,8 +0,0 @@
-class CSVParser {
-    public static void main(String[] args){
-        String line="Ananya Verma,RA2211003010123,CSE";
-        String[] parts=line.split(",");
-        if(parts.length!=3) System.out.println("Invalid Record");
-        else System.out.println("Name:"+parts[0]+" Roll No:"+parts[1]+" Dept:"+parts[2]);
-    }
-}
